9ad423ebe608d29d80ff60dcd4df930d186199be
fahadabbas817 <fahadabbas817@gmail.com>
2023-12-25 16:51:17 +0500

    Program to find the highest value of three variables

---
+++ Highest_value.php
@@ -0,0 +1,13 @@
+<?php
+	$var1 = 8;
+	$var2 = 10;
+	$var3 = 12;
+	$highest = $var1;
+	if($var2>$highest){
+	  $highest = $var2;
+	}
+	if($var3>$highest){
+	  $highest = $var3;
+	}
+	echo $highest
+?>
