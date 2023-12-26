3d4310cf5692108dbc16aed794475f81087f86c3
fahadabbas817 <fahadabbas817@gmail.com>
2023-12-26 23:50:57 +0500

    Question 1 solved by else if statements

--- PHP_Exercise_2.php
+++ PHP_Exercise_2.php
@@ -1,15 +1,26 @@
 <?php
-// Solving the question of printing the table of 2 using for loop
-$table =2;
-
-// For loop started from 1 which would run till 10
-for ($var=1;$var<=10 ; $var++ ){
-    // For every iteration the $table will be multiplied by the iteration value ($var) and its value will be assigned to answer variable
-  $answer = $var*$table;
-
-  echo"$table x  $var  = $answer\n";
-//   The value of $table will remain the same and of $var will be changing with the number of iterations. $answer will be calculated according to that $variable value and the table of 2 will be printed;
-}      
+// Variable intialized to 3
+$var = 3;
+   
+    // Checking by if else statement that value of $var matches with which number
+if($var==1){
+  echo 1;
+//   Condition not true so skipped
+}
+else if($var==2){
+  echo 2;
+  //   Condition not true so skipped
+}
+else if($var==3){
+  echo 3;
+  //   Condition  true so we will get 3 as an output and if statement will break here without checking next elseif and else statement
+}
+else if($var==4){
+  echo 4;
+}
+else {
+  echo "You were unable to find value";
+}
     
   
     
